95079b37b6eb5f897fc7e9c3c5af006332b283a2
gabrieldasilvadev <gabriel.dsilva@sptech.school>
2022-10-08 23:54:06 -0300

    Cria classe de conexao com o bd e configura acesso a azure

--- src/main/java/br/com/devices/db/Conexao.java
+++ src/main/java/br/com/devices/db/Conexao.java
@@ -4,10 +4,26 @@
  */
 package br.com.devices.db;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  *
  * @author gabri
  */
 public class Conexao {
+    
+    private JdbcTemplate connection;
+    
+    public Conexao() {
+        BasicDataSource datasource = new BasicDataSource();
+        
+        datasource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        datasource.setUrl("jdbc:sqlserver://projeto-device.database.windows.net/bd-device");
+        datasource.setUsername("device-admin");
+        datasource.setPassword("d3v1c6-password");
+        
+        connection = new JdbcTemplate(datasource);
+    }
     
 }
