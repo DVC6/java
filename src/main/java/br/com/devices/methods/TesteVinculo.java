660130796efd0103ce319dc06c6feccfccc91dea
rafaelmrapacci <rafael.rapacci@sptech.school>
2022-10-23 20:19:23 -0300

    Resolving merging conflicts

--- src/main/java/br/com/devices/methods/TesteVinculo.java
+++
@@ -1,9 +0,0 @@
-
-package br.com.devices.methods;
-
-public class TesteVinculo {
-    public static void main(String[] args) {
-        Vinculo vinculo = new Vinculo();
-        vinculo.Vincular("12345", "Machine-01-test", "Recepcao 1");
-    }
-}
