40c42daa5fd057d617f16237857b3b6ab504607d
Santos Junior <resantosjunior@deloitte.com>
2022-11-07 20:14:58 -0300

    Adicionando dependencias slack

--- src/main/java/br/com/devices/entities/App.java
+++ src/main/java/br/com/devices/entities/App.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.devices.entities;
-
+import org.json.JSONObject;
 import java.io.IOException;
 
 /**
--- src/main/java/br/com/devices/entities/Slack.java
+++ src/main/java/br/com/devices/entities/Slack.java
@@ -7,6 +7,9 @@ package br.com.devices.entities;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import org.json.JSONObject;
+import java.net.URI;
+import java.io.IOException;
 
 /**
  *
@@ -14,16 +17,16 @@ import java.net.http.HttpResponse;
  */
 public class Slack {
   private static HttpClient client = HttpClient.newHttpClient();
-  private static final String URL = "https://hooks.slack.com/services/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"
+  private static final String URL = "https://hooks.slack.com/services/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";
           
-  public static void sendMessage(JSONObject content){
+  public static void sendMessage(JSONObject content) throws IOException, InterruptedException{
     HttpRequest request = HttpRequest.newBuilder(
                           URI.create(URL))
                           .header("Accept", "application/json")
                           .POST(HttpRequest.BodyPublisher.ofString(content.toString()))
                           .build();
     
-    HttpResponse<String> response = client.send(resquest, HttpResponse.BodyHandlers.ofString())
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
     System.out.println(String.format("Status: %s", response.statusCode()));
     System.out.println(String.format("Response: %s", response.body()));
   }
